cacbbae320216871662f0adac1cdc9a0811fd776
Suresh_Gobi <sureshgobi34@gmail.com>
2023-11-06 19:29:05 +0530

    Monogodb setup

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
