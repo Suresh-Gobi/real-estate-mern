8eb9ec0dc74828ad5484293139c6f92791f896bf
Suresh_Gobi <sureshgobi34@gmail.com>
2023-11-13 14:09:16 +0530

    Signup updated

--- backend/controllers.js/auth.controller.js
+++ backend/controllers.js/auth.controller.js
@@ -1,11 +1,29 @@
 const User = require("../models/user.model.js");
+const bcrypt = require("bcrypt");
 
-const signup = (req, res) => {
-    const {username, email, password} = req.body;
+const signup = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
 
-    const newUser = new User({username, email, password});
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(409).json({ error: "Username already exists" });
+    }
+
+    const hashPassword = bcrypt.hashSync(password, 10);
+
+    const newUser = new User({ username, email, password: hashPassword });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully." });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 module.exports = {
-    signup
+  signup,
 };
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoutes = require("./routes/UserRoute.routes.js"); 
-const authRouters = require("./routes/auth.routes.js")
+const userRoutes = require("./routes/UserRoute.routes.js");
+const authRouters = require("./routes/auth.routes.js");
 
 dotenv.config();
 
@@ -16,8 +17,19 @@ mongoose
   });
 
 const app = express();
+app.use(bodyParser.json());
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRouters);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 const port = 5000;
 app.listen(port, () => {
